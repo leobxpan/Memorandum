92ba1f094b2adffb01533c3862707819d3880f0f
Caesar <pankobe24@vip.qq.com>
2015-05-04 09:02:06 +0800

    Initial Commit

---
+++ Memorandum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Memorandum
+//
+//  Created by Caesar on 15/5/4.
+//  Copyright (c) 2015年 Caesar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
